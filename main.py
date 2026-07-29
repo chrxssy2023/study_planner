23644f95985ec4d6b7259ca98040cc57bcb33b39
christina <cchristinaachen@gmail.com>
2026-07-29 15:03:16 +1200

    Querying the Database

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "study_planner.db"
+
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route("/")
@@ -8,14 +20,40 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/styles")
-def styles():
-    return render_template("styles.shtml")
-
-
 @app.route("/webpages")
-def webpages():
-    return render_template("webpages.html")
+def render_webpages():
+
+    query = "SELECT assignment_name, due_date FROM assignments"
+
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+
+    cur.execute(query)
+    tag_list = cur.fetchall()
+
+    con.close()
+
+    print(tag_list)
+
+    return render_template("webpages.html", tags=tag_list)
+
+
+@app.route("/styles")
+def render_styles():
+
+    query = "SELECT assignment_name, due_date FROM assignments"
+
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+
+    cur.execute(query)
+    tag_list = cur.fetchall()
+
+    con.close()
+
+    print(tag_list)
+
+    return render_template("styles.html", tags=tag_list)
 
 
 if __name__ == "__main__":
